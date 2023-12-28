eabdf62a7a9a730160865185c34e90a2493d5d96
RoyalPK04 <kumarpratyush3001@gmail.com>
2023-12-28 15:51:29 +0530

    update for about us

--- common.js
+++ common.js
@@ -1,28 +1,51 @@
-<script>
+
 // common.js
 document.addEventListener("DOMContentLoaded", function () {
     // Load header
-    fetch("header.html")
-        .then(response => response.text())
-        .then(html => document.getElementById("header-placeholder").innerHTML = html);
+    fetchContent("header.html", "header-placeholder");
 
     // Load sidebar
-    fetch("sidebar.html")
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("sidebar-placeholder").innerHTML = html;
-            initializeSidebar();
-        });
+    fetchContent("sidebar.html", "sidebar-placeholder", initializeSidebar);
 });
+
+function fetchContent(url, targetId, callback) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch ${url}: ${response.status} ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(html => {
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                targetElement.innerHTML = html;
+                if (callback && typeof callback === "function") {
+                    callback();
+                }
+            } else {
+                console.error(`Element with id '${targetId}' not found.`);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
 function initializeSidebar() {
     const toggleSidebarBtn = document.getElementById("toggle-sidebar-btn");
     const sidebar = document.getElementById("sidebar");
 
-    // Add event listener to the toggle button
-    toggleSidebarBtn.addEventListener("click", function () {
-        // Toggle the 'active' class on the sidebar to show/hide it
-        sidebar.classList.toggle("active");
-    });
+    if (toggleSidebarBtn && sidebar) {
+        // Add event listener to the toggle button
+        toggleSidebarBtn.addEventListener("click", function () {
+            // Toggle the 'active' class on the sidebar to show/hide it
+            sidebar.classList.toggle("active");
+        });
+    } else {
+        console.error("Sidebar elements not found.");
+    }
 }
-</script>
+
+
+
